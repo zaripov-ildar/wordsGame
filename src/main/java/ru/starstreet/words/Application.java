2af6f202bfc016b68166dc9cb0bc800bba1b765a
Ildar Zaripov <zariil@yandex.ru>
2022-06-04 16:25:05 +0500

    Homework(JFX) game of words

--- src/main/java/ru/starstreet/words/Application.java
+++ src/main/java/ru/starstreet/words/Application.java
@@ -10,7 +10,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Words-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 550);
+        Scene scene = new Scene(fxmlLoader.load(), 400, 525);
         stage.setResizable(false);
         stage.setTitle("Words");
         stage.setScene(scene);
